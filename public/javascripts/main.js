2328e6e7792235fe6b06615c8b56c0f85de8f9cb
raultorres2603 <raultorres2603@gmail.com>
2021-01-26 16:51:12 +0100

    Better layout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    $("#login").fadeIn(3000);
+    $("#logo").fadeIn(1000);
+    setTimeout(function() {
+        $('#username').fadeIn(1500);
+        $('#password').fadeIn(1500);
+    },1000);
 });
+
+function compLog(username, password) {
+    if (username.length > 0 && password.length > 0) {
+        $('#entrar').fadeIn(1000);
+    } else {
+        $('#entrar').fadeOut(1000);
+    }
+}
